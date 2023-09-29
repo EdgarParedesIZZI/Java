381549e6233d3d56b56d6a4ae88841ea65313e46
Edgar Paredes <p-eparedese@izzi.mx>
2023-09-29 13:36:34 -0600

    se modifica la clase Edgar.java

--- ConsoleJava/src/ClaseEdgar.java
+++ ConsoleJava/src/ClaseEdgar.java
@@ -11,6 +11,9 @@
 public class ClaseEdgar {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        System.out.println("Hello, World1!");
+        System.out.println("Hello, World2!");
+        System.out.println("Hello, World3!");
     }
     
 }
